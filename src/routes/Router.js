513c1889ba10e1633e0ba851e8445a6545c6fe36
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2018-01-27 14:58:24 -0500

    feat(Router): Create base router and router service

---
+++ src/routes/Router.js
@@ -0,0 +1,17 @@
+import { TabNavigator } from 'react-navigation';
+
+import Home from '../views/Home';
+import Dev from '../views/Dev';
+
+import { createTabStyles } from '../services/routers.service';
+import CONSTANTS from '../services/constants';
+import COLORS from '../services/colors.constants';
+
+const TAB_POSITION = CONSTANTS.TABS.BOTTOM_POSITION;
+const ACTIVE_TINT_COLOR = COLORS.RUBY;
+const tabStyle = createTabStyles(TAB_POSITION, ACTIVE_TINT_COLOR)
+
+export default TabNavigator({
+  Home: {screen: Home},
+  Dev: {screen: Dev},
+}, tabStyle);

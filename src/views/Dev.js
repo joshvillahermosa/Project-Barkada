63dc5d231f576d07b3b1fd3c5b568e40d369e751
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2018-01-27 14:57:11 -0500

    feat(Dev): Create basic Dev View

---
+++ src/views/Dev.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native-elements';
+
+export default class Dev extends Component {
+  render() {
+    return (<Text>@todo Dev Page</Text>)
+  }
+}

d6423b18e30889fe879d47f6dd81fdb21ea8047d
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2018-01-28 16:37:08 -0500

    feat(Dev): Integrate configs for dev view

---
+++ src/configs/dev.config.js
@@ -0,0 +1,40 @@
+/**
+ * @file Dev View Configuration
+ * @description General object to configure dev view. Hopefully one day this will
+ * be in a XHR API
+ */
+export default [
+  {
+    name: 'App State',
+    view: null,
+    enabled: false
+  }, {
+    name: 'Contact Import',
+    view: null,
+    enabled: false
+  }, {
+    name: 'Contact Viewer',
+    view: null,
+    enabled: false
+  }, {
+    name: 'User State',
+    view: null,
+    enabled: false
+  }, {
+    name: 'Event State',
+    view: null,
+    enabled: false
+  }, {
+    name: 'Ad Display',
+    view: null,
+    enabled: false
+  }, {
+    name: 'Local Database',
+    view: null,
+    enabled: false
+  }, {
+    name: 'SMS Tester',
+    view: null,
+    enabled: false
+  }
+];

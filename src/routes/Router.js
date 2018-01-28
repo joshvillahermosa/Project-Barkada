de2a373f96cbf3c9d57c4f4a379b0855118ec14c
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2018-01-28 16:40:31 -0500

    fix(Router): Fix colors import statement

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -5,7 +5,7 @@ import Dev from '../views/Dev';
 
 import { createTabStyles } from '../services/routers.service';
 import CONSTANTS from '../services/constants';
-import COLORS from '../services/colors.constants';
+import COLORS from '../styles/colors.constants';
 
 const TAB_POSITION = CONSTANTS.TABS.BOTTOM_POSITION;
 const ACTIVE_TINT_COLOR = COLORS.RUBY;

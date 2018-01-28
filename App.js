661611b66cac37e0eaead459fa7e67847ee216e3
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2018-01-28 16:38:46 -0500

    refactor(App): Removed commented and unused components

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import Router from'./src/routes/Router';
 
@@ -10,13 +10,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-// @todo Remove when we no longer need a reference
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
